0b0387b3617651521f3f61f601907bac93e0e978
Renae Ricketts <rricket4@umd.edu>
2026-02-23 12:18:28 -0500

    Removed benchmark commands from ingestion script. Not sure how those work yet.

--- scripts/lfw_ingestion_script.py
+++ scripts/lfw_ingestion_script.py
@@ -3,9 +3,4 @@ import tensorflow_datasets as tfds
 
 ds = tfds.load('lfw', split='train')
 assert isinstance(ds, tf.data.Dataset)
-
-ds = ds.batch(32).prefetch(1)
-
-tfds.benchmark(ds, batch_size=32)
-tfds.benchmark(ds, batch_size=32)  # Second epoch much faster due to auto-caching
 print(ds)
