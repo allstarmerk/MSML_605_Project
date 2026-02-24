3aa4dbd84d628e3317e7397e852861a72288c4a8
allstarmerk <john.n.sheikh@gmail.com>
2026-02-24 08:32:47 -0500

    fast vectorized cosine func

--- src/similarity.py
+++ src/similarity.py
@@ -3,3 +3,19 @@
 #then faster vectorized 
 
 
+import numpy as np
+
+#using vectorized faster version then python loop for cosine and euclidean
+def cosine_similarity(a, b):
+    norm_a = np.linalg.norm(a, axis=1, keepdims=True) #keep dim perventing the (N,) to be considered (1,N) like broadcasting would normaly do. This makes it become (N,1) for broadcasting
+    norm_b = np.linalg.norm(b, axis=1, keepdims=True)
+
+    norm_a = np.where(norm_a == 0, 1e-10, norm_a) #doing so no zero vectors 
+    norm_b = np.where(norm_b == 0, 1e-10, norm_b)
+
+    a_unit = a / norm_a #(N, D)
+    b_unit = b / norm_b # same as above N,D
+
+    return np.einsum("nd , nd to N", a_unit, b_unit) #(N,)
+
+
