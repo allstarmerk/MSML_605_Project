52ab8d6046a057ef48f6c3d5746cc20788082ba8
allstarmerk <john.n.sheikh@gmail.com>
2026-02-25 03:48:44 -0500

    attemting to load data set from sk

--- scripts/lfw_ingestion_script.py
+++ scripts/lfw_ingestion_script.py
@@ -3,13 +3,14 @@ import os
 
 
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 sys.path.insert(0, root_path)
 
 from src.data_ingestion import (
     load_config,
-    load_lfw
+    load_lfw,
+    save_mainifest
 )
+from src.pair_generation import ( generate_all_splits)
 
 
 """"
@@ -17,7 +18,13 @@ ds = tfds.load('lfw', split='train')
 assert isinstance(ds, tf.data.Dataset)
 print(ds)
 """
-
+# load config
 data_config = load_config()
+#load LFW and split
 
 data_splits, data_manifest = load_lfw(data_config)
+#save manifest
+
+#generate pairs
+
+#then summary to printout what was produced
