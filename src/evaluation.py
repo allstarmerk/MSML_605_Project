2a261af4716d2e50dd97937c2a16f1b747102843
allstarmerk <john.n.sheikh@gmail.com>
2026-03-23 21:12:50 -0400

    starting evaluation py file cosine func

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,9 @@
-# evaluation py file
+# will take pairs, images, and threshold and produce metrics
+import numpy as np
+from src.similarity import cosine_similarity, flatten_embedding
+
+def score_pairs(images, pairs):
+    #compute the cosine similarity score for every pair
+    a = flatten_embedding(images[pairs])
+    b = flatten_embedding(images[pairs[:, 1] ])
+    return cosine_similarity(a, b) #higher = more similar, lower = less similar?
